cd9b5399a24eed7b9ba474ac20407c19abb5a57f
Ethan Kaseff <ethan@kaseff.com>
2022-04-28 12:23:25 -0500

    setup backend

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+FLASK_APP=colors.py
+FLASK_ENV=development
+DATABASE_URL=postgresql://colors_app:colors_app@localhost/colors_app
---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from app.models import db
+from flask import Flask
+from flask_migrate import Migrate
+import os
+
+app = Flask(__name__)
+app.config.from_mapping({
+    'SQLALCHEMY_DATABASE_URI': os.environ.get('DATABASE_URL'),
+    'SQLALCHEMY_TRACK_MODIFICATIONS': False,
+})

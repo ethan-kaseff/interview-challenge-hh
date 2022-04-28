0df0834cab011106dca1035cab95adbf60d5b9e7
Ethan Kaseff <ethan@kaseff.com>
2022-04-28 14:32:19 -0500

    update seed path

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,22 @@ from app.models import db
 from flask import Flask
 from flask_migrate import Migrate
 import os
+from .seeds import seed_commands
 
+# Initialize App
 app = Flask(__name__)
+
+# Config 
 app.config.from_mapping({
     'SQLALCHEMY_DATABASE_URI': os.environ.get('DATABASE_URL'),
     'SQLALCHEMY_TRACK_MODIFICATIONS': False,
 })
+
+# Give us seed command
+app.cli.add_command(seed_commands)
+
+# Connect App with database
 db.init_app(app)
+
+# Migrate using Flask/Alembic
 Migrate(app, db)

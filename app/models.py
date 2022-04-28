3cb4fa178a33a791692050639c8106c070c8b1ee
Ethan Kaseff <ethan@kaseff.com>
2022-04-28 12:59:05 -0500

    create migration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Color(db.Model):
+    __tablename__ = 'colors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    hex_code = db.Column(db.String(7), nullable=False)
